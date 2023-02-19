fb4ea07642bf8b9b3a4a2f88fa078a3a6f86450c
Hamza <hamzaensi@gmail.com>
2023-02-19 11:48:57 +0100

    string

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+/** 
 
 function calculer(a , b)
 {
@@ -47,3 +47,46 @@ document.getElementById("zone").onmousemove = into;
 document.getElementById("zone").onmouseout = out;
 document.getElementById("zone").onkeydown = bgColor;
 
+
+*/
+
+
+/**
+ * String 
+ */
+
+msg = "hello"; 
+/*
+msg1='';
+for ( i = 0; i < msg.length; i++) {
+    
+    
+    msg1 +=msg[msg.length-i-1];
+}
+
+console.log(msg1)
+
+console.log(msg.toUpperCase());
+console.log(msg.toLowerCase());
+msg = "Bonjour ISET Tataouine Bonjour ISET Bonjour";
+
+
+
+msg1= msg.replace('ISET','ISAM');
+console.log(msg.charAt(9));
+
+let str = "Please locate where locate 'locate' locate locate occurs!";
+let p = str.indexOf("locate",str.indexOf("locate")+1);
+
+//console.log(p);
+*/
+
+
+
+
+
+let str = "Please locate where 'locate' occurs!";
+p = str.search("locate");
+
+
+console.log(p);
